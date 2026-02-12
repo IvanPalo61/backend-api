e2be24b57dccde9fe3113ab3953c758041067cc4
ANGEL IVAN PALOMARES MORALES <124049019@upq.edu.mx>
2026-02-11 20:27:09 -0600

    Practica terminada
    se hicieron cambios para busqueda a traves de palabras o incluso letras

--- src/controllers/externalController.js
+++ src/controllers/externalController.js
@@ -2,37 +2,142 @@ const pool = require('../config/db');
 
 const poblarProductos = async (request, response) => {
     try {
-        // Fetch FakeStoreApi
-        const apiFetch = await fetch('http://fakestoreapi.com/products');
+       
+        const apiFetch = await fetch('https://fakestoreapi.com/products');
         const products = await apiFetch.json();
 
         let inserciones = 0;
-        // Destructurar el objeto
-        for(const product of products){
-            const { title, price, description, image} = product;
+
+        for (const product of products) {
+
+            const {
+                title,
+                price,
+                description,
+                image,
+                category
+            } = product;
 
             const stock = Math.floor(Math.random() * 50) + 1;
 
-            const query = `
-                INSERT INTO productos
-                (nombre, precio, stock, descripcion, imagen_url)
-                VALUES ($1, $2, $3, $4, $5)
-            `
+            // ===============================
+            // 1. Buscar si ya existe categoría
+            // ===============================
+            const buscarCategoria = `
+                SELECT id FROM categoria
+                WHERE nombre = $1
+            `;
 
-            await pool.query(query, [title, price, stock, description, image]);
+            const categoriaResult = await pool.query(
+                buscarCategoria,
+                [category]
+            );
+
+            let idCategoria;
+
+            // ===============================
+            // 2. Si no existe → crearla
+            // ===============================
+            if (categoriaResult.rows.length === 0) {
+
+                const insertarCategoria = `
+                    INSERT INTO categoria (nombre)
+                    VALUES ($1)
+                    RETURNING id
+                `;
+
+                const nuevaCategoria = await pool.query(
+                    insertarCategoria,
+                    [category]
+                );
+
+                idCategoria = nuevaCategoria.rows[0].id;
+
+            } else {
+                // Ya existe
+                idCategoria = categoriaResult.rows[0].id;
+            }
+
+            // ===============================
+            // 3. Insertar producto
+            // ===============================
+            const insertarProducto = `
+                INSERT INTO productos
+                (nombre, precio, stock, descripcion, imagen_url, id_categoria)
+                VALUES ($1, $2, $3, $4, $5, $6)
+            `;
+
+            await pool.query(insertarProducto, [
+                title,
+                price,
+                stock,
+                description,
+                image,
+                idCategoria
+            ]);
 
             inserciones++;
         }
-        response.status(200).json(
-            {
-                mensaje: "Carga masiva exitosa", 
-                cantidad: inserciones
-            }
-        );
+
+        response.status(200).json({
+            mensaje: "Carga masiva exitosa",
+            cantidad: inserciones
+        });
+
     } catch (error) {
-        console.log(`Error: ${error}`);
-        response.status(500).json({error: error.message})
+        console.error("Error:", error);
+        response.status(500).json({
+            error: error.message
+        });
     }
 };
+const getProductos = async (req, res) => {
+  try {
 
-module.exports = { poblarProductos };
+    const { q } = req.query;
+
+    if (!q || q.trim() === "") {
+      return res.status(400).json({
+        error: "Debes enviar un parámetro de búsqueda: ?q=palabra"
+      });
+    }
+
+    let sql = "SELECT * FROM productos WHERE nombre ILIKE $1";
+    let values = [`%${q}%`];
+
+    const result = await pool.query(sql, values);
+
+    res.json(result.rows);
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error al leer BD' });
+  }
+};
+const getCategoria = async (req, res) => {
+  try {
+
+    const { k } = req.query;
+
+    if (!k || k.trim() === "") {
+      return res.status(400).json({
+        error: "Debes enviar un parámetro de búsqueda: ?q=palabra"
+      });
+    }
+
+    let sql = "SELECT * FROM categoria WHERE nombre ILIKE $1";
+    let values = [`%${k}%`];
+
+    const result = await pool.query(sql, values);
+
+    res.json(result.rows);
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error al leer BD' });
+  }
+};
+
+
+
+module.exports = { poblarProductos, getProductos, getCategoria };
--- src/routes/productoRoute.js
+++ src/routes/productoRoute.js
@@ -1,7 +1,18 @@
 const express = require('express');
-const { poblarProductos } = require('../controllers/externalController');
+
+const {
+  poblarProductos,
+  getProductos,
+  getCategoria
+} = require('../controllers/externalController');
+
 const router = express.Router();
 
 router.post('/poblar', poblarProductos);
+
+router.get('/productos', getProductos);
+
+
+router.get('/categoria', getCategoria);
 
 module.exports = router;
