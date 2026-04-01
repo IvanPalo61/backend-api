4a299425c62b651dedc70d670e9a677fe6721b40
ANGEL IVAN PALOMARES MORALES <124049019@upq.edu.mx>
2026-03-31 23:52:42 -0600

    yes

--- src/config/db.js
+++ src/config/db.js
@@ -2,13 +2,14 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASS,
-    port: process.env.DB_PORT,
-    host: process.env.DB_HOST
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
-pool.on('connnect', () => console.log('Conexion exitosa'));
+pool.on('connect', () => {
+  console.log('Conexión exitosa a PostgreSQL');
+});
 
 module.exports = pool;
